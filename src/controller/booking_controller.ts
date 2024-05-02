9a210fd88b9f833fa8925e791c7926bd90b7041f
Nick Yang <nick10811@gmail.com>
2024-05-02 13:34:05 +0800

    use class instead

--- src/controller/booking_controller.ts
+++ src/controller/booking_controller.ts
@@ -1,6 +1,37 @@
 import moment from "moment-timezone";
 import { BookingObj, Payload } from "../model";
 
+export interface IBookingController {
+    makeObj(payload: Payload): BookingObj;
+}
+
+export class BookingController implements IBookingController {
+    makeObj(payload: Payload): BookingObj {
+        const baseURL = payload.bookerUrl;
+        const rescheduleURI = baseURL + "/reschedule/" + payload.uid;
+        const cancelURI = baseURL + "/booking/" + payload.uid + "?cancel=true&allRemainingBookings=false";
+
+        return {
+            bookingId: payload.bookingId.toString(),
+            status: payload.status,
+            greeting: "Hello " + payload.responses.name?.value,
+            location: payload.location,
+            duration: this.makeDurationString(payload.startTime, payload.endTime, payload.organizer.timeZone),
+            timezone: payload.organizer.timeZone,
+            attendee: payload.responses.name?.value ?? "unknown name",
+            rescheduleURI: rescheduleURI,
+            cancelURI: cancelURI,
+        };
+    }
+
+    makeDurationString(startTime: string, endTime: string, timezone: string): string {
+        const start = moment.utc(startTime, true).tz(timezone).format('YYYY-MM-DD HH:mm');
+        const end = moment.utc(endTime, true).tz(timezone).format('HH:mm');
+        return `${start} - ${end}`;
+    }
+}
+
+
 function makeDurationString(startTime: string, endTime: string, timezone: string): string {
     const start = moment.utc(startTime, true).tz(timezone).format('YYYY-MM-DD HH:mm');
     const end = moment.utc(endTime, true).tz(timezone).format('HH:mm');
