1cd957156e3ec179bc436de513c22d7c32ed59b1
Nick Yang <nick10811@gmail.com>
2024-05-03 20:21:05 +0800

    refine coding convention

--- src/service/google_service.test.ts
+++ src/service/google_service.test.ts
@@ -1,61 +1,64 @@
-import { describe, test, assert, expect } from 'vitest';
+import { describe, test, expect } from 'vitest';
 import { GoogleService } from './google_service';
 import config from '../config/config';
 import { JWTInput } from 'google-auth-library/build/src/auth/credentials';
 
-describe('GoogleService_getAuthToken', () => {
-    test('getAuthToken', async () => {
+describe('GoogleService.getAuthToken', () => {
+    test('ok', async () => {
         // arrange
-        config.GOOGLE_SERVICE_ACCOUNT_EMAIL = "whatever";
-        config.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY = "whatever";
+        config.GOOGLE_SERVICE_ACCOUNT_EMAIL = 'whatever';
+        config.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY = 'whatever';
         const srv = new GoogleService();
 
         // act
         const got = srv.getAuthToken();
 
-        // assert
-        assert.equal((got.jsonContent as JWTInput).client_email, "whatever");
-        assert.equal((got.jsonContent as JWTInput).private_key, "whatever");
+        // expect
+        expect((got.jsonContent as JWTInput).client_email).toBe('whatever');
+        expect((got.jsonContent as JWTInput).private_key).toBe('whatever');
     });
 });
 
-describe('GoogleService_getSheetData', () => {
+describe('GoogleService.getSheetData', () => {
     test.skip('skip ci for real case', async () => {
         // arrange
-        config.GOOGLE_SERVICE_ACCOUNT_EMAIL = "wahtever";
-        config.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY = "whatever";
+        config.GOOGLE_SERVICE_ACCOUNT_EMAIL = 'whatever';
+        config.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY = 'whatever';
         const srv = new GoogleService();
-        const spreadsheetId = 'whatever';
+        const spreadsheetId = '1348FLkrFKgTuBClszAG30TLIY2pKtCVeEZm5SzVPURQ';
         const sheetName = 'reservations';
 
         // act
         const got = await srv.getSheetData(spreadsheetId, sheetName);
 
-        // assert
-        assert.equal((got as string[][])[0].length, 4);
-        assert.equal((got as string[][])[0][0], 'Name');
-        assert.equal((got as string[][])[0][1], 'Location');
-        assert.equal((got as string[][])[0][2], 'Datetime');
-        assert.equal((got as string[][])[0][3], 'Timezone');
+        // expect
+        expect(got).toBeInstanceOf(Array);
+        expect((got as string[][])[0].length).toEqual(6);
+        expect((got as string[][])[0][0]).toEqual('ID');
+        expect((got as string[][])[0][1]).toEqual('Name');
+        expect((got as string[][])[0][2]).toEqual('Location');
+        expect((got as string[][])[0][3]).toEqual('Datetime');
+        expect((got as string[][])[0][4]).toEqual('Timezone');
+        expect((got as string[][])[0][5]).toEqual('Status');
     });
-
 });
 
 describe('GoogleService_appendSheetData', () => {
     test.skip('skip ci for real case', async () => {
         // arrange
-        config.GOOGLE_SERVICE_ACCOUNT_EMAIL = "whatever";
-        config.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY = "whatever";
+        config.GOOGLE_SERVICE_ACCOUNT_EMAIL = 'whatever';
+        config.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY = 'whatever';
         const srv = new GoogleService();
-        const spreadsheetId = 'whatever';
+        const spreadsheetId = '1348FLkrFKgTuBClszAG30TLIY2pKtCVeEZm5SzVPURQ';
         const sheetName = 'reservations';
         const values = [['demo - John Doe', 'New York', '2021-01-01 10:00', 'America/New_York']];
 
         // act
         const got = await srv.appendSheetData(spreadsheetId, sheetName, values);
 
-        // assert
-        assert.equal(got.spreadsheetId, "whatever");
+        // expect
+        expect(got.spreadsheetId).toBe("1348FLkrFKgTuBClszAG30TLIY2pKtCVeEZm5SzVPURQ");
+        expect(got.updates).not.toBeUndefined();
     });
 
 });
@@ -75,11 +78,10 @@ describe('GoogleService.updateSheetRow', () => {
         const got = await srv.updateSheetRow(spreadsheetId, sheetName, range, values);
 
         // expect
-        expect(got.spreadsheetId).toBe("1348FLkrFKgTuBClszAG30TLIY2pKtCVeEZm5SzVPURQ");
+        expect(got.spreadsheetId).toBe('1348FLkrFKgTuBClszAG30TLIY2pKtCVeEZm5SzVPURQ');
         expect(got.updatedCells).toBe(3);
         expect(got.updatedColumns).toBe(3);
-        expect(got.updatedRange).toBe("reservations!A2:C2");
+        expect(got.updatedRange).toBe('reservations!A2:C2');
         expect(got.updatedRows).toBe(1);
     });
-
 });
