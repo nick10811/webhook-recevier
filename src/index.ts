bdd33a2c2da8e8943a2d2b91ca15209d06e8dd7a
Nick Yang <nick10811@gmail.com>
2024-05-07 15:47:37 +0800

    setup i18n

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,34 @@ import { middleware, webhook, HTTPFetchError } from '@line/bot-sdk';
 import { GoogleService, LineService, lineMiddlewareConfig, lineClientConfig } from './service';
 import { BookingController, CalEventController, CalServices, LineEventController, SheetsController } from './controller';
 import { CalResponse } from './model';
+import i18next from 'i18next';
+import Backend from 'i18next-node-fs-backend';
+import i18nextMiddleware from 'i18next-http-middleware';
 
 // create Express app
 const app = express();
+
+// configure i18n
+i18next
+    .use(Backend)
+    .use(i18nextMiddleware.LanguageDetector)
+    .init({
+        fallbackLng: 'en',
+        preload: ['en', 'zh-tw'],
+        lowerCaseLng: true,
+        backend: {
+            loadPath: 'locale/{{lng}}.json',
+        },
+    });
+app.use(i18nextMiddleware.handle(i18next));
 
 // line webhook
 app.post(
     '/linewebhook',
     middleware(lineMiddlewareConfig),
     async (req: Request, res: Response): Promise<Response> => {
+        const lang = req.query.lang as string;
+        i18next.changeLanguage(lang);
         console.log(`received a webhook event (line): ${JSON.stringify(req.body)}`)
 
         const callbackRequest: webhook.CallbackRequest = req.body;
@@ -57,6 +76,8 @@ app.use(bodyParser.json());
 app.post(
     '/calwebhook',
     async (req: Request, res: Response): Promise<Response> => {
+        const lang = req.query.lang as string;
+        i18next.changeLanguage(lang);
         console.log(`received a webhook event (cal.com): ${JSON.stringify(req.body)}`)
 
         const callbackRequest: CalResponse = req.body;
